a8fb62be38589ad18d8082de9b4f8a6d43a5ee0e
Jason W. Plummer <vhsjwp01@gmail.com>
2022-12-29 11:17:53 -0600

    MAC Allow List is now its own repo

--- overlay.sh
+++ overlay.sh
@@ -108,49 +108,6 @@ done
 
 echo
 
-while [ -z "${enable_mac_allow_db}" ]; do
-    read -p "Do you have a MAC allow list database (y/n)?: " enable_mac_allow_db
-    enable_mac_allow_db=$(echo "${enable_mac_allow_db}" | tr '[A-Z]' '[a-z]' | sed -e 's/[^(y|n)]//g')
-
-    case ${enable_mac_allow_db} in
-
-        y)
-            echo
-
-            while [ -z "${mac_allow_db}" ]; do
-                read -p "Enter the MAC allow DB name: " mac_allow_db
-            done
-
-            echo
-
-            while [ -z "${db_host}" ]; do
-                read -p "Enter the DB Hostname for DB '${mac_allow_db}': " db_host
-            done
-
-            echo
-
-            while [ -z "${db_port}" ]; do
-                read -p "Enter the Port Number for DB host '${db_host}': " db_port
-            done
-
-            echo
-
-            while [ -z "${db_user}" ]; do
-                read -p "Enter the Username with grants to DB '${mac_allow_db}': " db_user
-            done
-
-            echo
-
-            while [ -z "${db_password}" ]; do
-                read -p "Enter the Password for DB username '${db_user}': " db_password
-            done
-
-        ;;
-
-    esac
-
-done
-
 # Find all files
 this_dir=$(realpath -L $(dirname "${0}"))
 
@@ -179,10 +136,6 @@ if [ -d "${this_dir}/overlay" ]; then
 
             wifi_ap_config)
                 copy_command="sed -e 's|::BRIDGE_IFNAME::|${bridge_ifname}|g' -e 's|::BRIDGE_IP::|${bridge_ip}|g' -e 's|::BRIDGE_GATEWAY::|${bridge_gateway}|g' -e 's|::BRIDGE_SUBNET::|${bridge_subnet}|g' -e 's|::MOBILITY_DOMAIN::|${mobility_domain}|g' -e 's|::FT_AES_KEY::|${ft_aes_key}|g' '${overlay_file}' > '${target_path}/${target_file}'"
-            ;;
-
-            mac_allow_list.conf)
-                copy_command="sed -e 's|::MAC_ALLOW_DB::|${mac_allow_db}|g' -e 's|::DB_HOST::|${db_host}|g' -e 's|::DB_PORT::|${db_port}|g' -e 's|::DB_USER::|${db_user}|g' -e 's|::DB_PASSWORD::|${db_password}|g' '${overlay_file}' > '${target_path}/${target_file}'"
             ;;
 
             *)
@@ -215,9 +168,3 @@ fi
 #./overlay/etc/default/wifi_ap_config:bridge_gateway="::BRIDGE_GATEWAY::"
 #./overlay/etc/default/wifi_ap_config:bridge_subnet="::BRIDGE_SUBNET::"
 #
-## Needs to be replaced with values
-#./overlay/etc/mac_allow_list/mac_allow_list.conf:db="::MAC_ALLOW_DB::"
-#./overlay/etc/mac_allow_list/mac_allow_list.conf:db_host="::DB_HOST::"
-#./overlay/etc/mac_allow_list/mac_allow_list.conf:db_port="::DB_PORT::"
-#./overlay/etc/mac_allow_list/mac_allow_list.conf:db_user="::DB_USER::"
-#./overlay/etc/mac_allow_list/mac_allow_list.conf:db_password="::DB_PASSWORD::"
